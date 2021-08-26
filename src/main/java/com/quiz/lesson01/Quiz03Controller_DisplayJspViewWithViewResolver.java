679e0a57c9ef20f7bbf7779295ada1bce9b9f33b
301-05\User <boyboy1962@gmail.com>
2021-08-26 19:04:11 +0900

    어제 수요일에 빼먹은 주석 정리

--- src/main/java/com/quiz/lesson01/Quiz03Controller_DisplayJspViewWithViewResolver.java
+++ src/main/java/com/quiz/lesson01/Quiz03Controller_DisplayJspViewWithViewResolver.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Quiz03Controller_DisplayJspViewWithViewResolver {
 	
-	//http://localhost/lesson01/quiz03/1
+	// 요청 URL : http://localhost/lesson01/quiz03/1
 	@RequestMapping("/lesson01/quiz03/1")
 	public String ex02() {
 		
