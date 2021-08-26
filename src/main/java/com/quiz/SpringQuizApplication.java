f3fe29e3252be90fd3530643a4c5da4c48115feb
301-05\User <boyboy1962@gmail.com>
2021-08-26 21:48:22 +0900

    목요일 마지막 교시 수업
    해당 내용은 boyboy1962/spring_example에 올린내용과 동일하니 참고하자
    오늘은 바뻐 그냥간다.

--- src/main/java/com/quiz/SpringQuizApplication.java
+++ src/main/java/com/quiz/SpringQuizApplication.java
@@ -1,17 +1,8 @@
 package com.quiz;
 
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-@MapperScan(basePackages = "com.quiz.*")
 @SpringBootApplication
 public class SpringQuizApplication {
 
@@ -19,14 +10,5 @@ public class SpringQuizApplication {
 		SpringApplication.run(SpringQuizApplication.class, args);
 	}
 
-	@Bean 
-	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource);
-		
-		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml"); // 약속된 경로이기에 무족건 이렇게 써야한다.
-		sessionFactory.setMapperLocations(res);
 
-		return sessionFactory.getObject();
-	}
 }
---
+++ src/main/java/com/quiz/config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package com.quiz.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@MapperScan(basePackages = "com.quiz.*")
+@Configuration
+public class DatabaseConfig {
+
+	@Bean 
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml"); // 약속된 경로이기에 무족건 이렇게 써야한다.
+		sessionFactory.setMapperLocations(res);
+
+		return sessionFactory.getObject();
+	}
+	
+}
